39afe243405b8451ff55ac15ada3bd4199802075
cjihrig <cjihrig@gmail.com>
2015-03-02 14:42:15 -0500

    update to Lab 5.x.x and Code 1.x.x

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 // Load modules
 
+var Code = require('code');
 var Lab = require('lab');
 var Nigel = require('..');
 var Vise = require('vise');
@@ -15,7 +16,7 @@ var internals = {};
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
-var expect = Lab.expect;
+var expect = Code.expect;
 
 
 describe('compile()', function () {
